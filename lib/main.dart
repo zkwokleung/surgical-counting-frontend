388f68c2ed5edf65b5adcc067ee76fbebe74688c
Andrew SZE-TO <zkwokleung@gmail.com>
2024-02-15 22:18:50 +0800

    feat: Testing Camera screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
@@ -5,9 +6,15 @@ import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
-  final settingsController = SettingsController(SettingsService());
+  // * Initialize plugins
+  WidgetsFlutterBinding.ensureInitialized();
 
+  // * Camera
+  final cameras = await availableCameras();
+
+  // * Settings
+  final settingsController = SettingsController(SettingsService());
   await settingsController.loadSettings();
 
-  runApp(MyApp(settingsController: settingsController));
+  runApp(App(settingsController: settingsController, camera: cameras.first));
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,19 +1,20 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:surgical_counting/src/screens/detection_screen.dart';
 
-import 'sample_feature/sample_item_details_view.dart';
-import 'sample_feature/sample_item_list_view.dart';
 import 'settings/settings_controller.dart';
-import 'settings/settings_view.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({
+class App extends StatelessWidget {
+  const App({
     super.key,
     required this.settingsController,
+    required this.camera,
   });
 
   final SettingsController settingsController;
+  final CameraDescription camera;
 
   @override
   Widget build(BuildContext context) {
@@ -41,13 +42,9 @@ class MyApp extends StatelessWidget {
               settings: routeSettings,
               builder: (BuildContext context) {
                 switch (routeSettings.name) {
-                  case SettingsView.routeName:
-                    return SettingsView(controller: settingsController);
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
-                  case SampleItemListView.routeName:
+                  case DetectionScreen.routeName:
                   default:
-                    return const SampleItemListView();
+                    return DetectionScreen(camera: camera);
                 }
               },
             );
